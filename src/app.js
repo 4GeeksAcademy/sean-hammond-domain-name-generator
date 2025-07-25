f23119211af9c7c928720c281aa7c4e37a342184
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-25 00:22:14 +0000

    still working on for loops

--- src/app.js
+++ src/app.js
@@ -1,47 +1,51 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/pexelsFreeScrabble.png";
 import "./assets/img/4geeks.ico";
 
 //declaring the arrays
-let possList = ['My', 'Their', 'Yalls']; // possessives
-let adjList = ['Fun', 'Cool', 'Blue'];
-let nounList = ['Pizza', 'Book', 'Jedi'];
-let extList = ['com', 'us', 'me', 'va', 'cat']
-
-// getting max array length to avoid hard-code
-let arrayLimit = Math.min(possList, adjList, nounList);
+let possList = ["My", "Their", "Yalls"]; // possessives
+let adjList = ["Fun", "Cool", "Blue"];
+let nounList = ["Pizza", "Book", "Jedi"];
+let extList = ["com", "us", "me", "va", "cat"];
 
 // Random integer generator
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-};
-
+}
 
 let text = "";
 
-// Looping through an array
-let iLoop = () => {
-  for (let i = 0; i < arrayLimit; i++) {
-    text = text + possList[i] + adjList(j);
-    document.getElementById(".listHere").innerHTML = text;
+
+// Declaring inner for loops
+
+let possLoop = () => {
+  for (let i = 0; i < possList.length; i++) {
+    adjLoop(i);
   }
 };
 
-let jLoop = () => {
-  iLoop();
-}
+let adjLoop = () => {
+  for (let j = 0; j < adjList.length; j++) {
+    return possLoop(j);
+  }
+};
 
-let kLoop = () => {
-  jLoop();
-} 
+// the outer loop
+let nounLoop = () => {
+  for (let k = 0; k < adjList.length; k++) {
+    jText = iText + possList[i] + adjList(i);
+    return iText;
+  }
+};
 
-window.onload = function() {
+// document.getElementById(".listHere").innerHTML = text;
+
+window.onload = function () {
   //write your code here
   possWord = getRandomIntInclusive(1, possessiveList.length);
   adjWord = getRandomIntInclusive(1, adjList.length);
