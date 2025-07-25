7d137a9c02adff51e808637fb90f5df18fa1c2ee
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-07-25 18:28:44 +0000

    cleaning up the code

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ window.onload = function () {
   // Declaring the arrays
   let possList = ["My", "Their", "Yalls"]; // possessives
   let adjList = ["Fun", "Cool", "Blue"];
-  let nounList = ["Pizza", "Book", "Jedi"];
+  let nounList = ["Pet", "Guava", "Jedi"];
   let extList = ["com", "us", "me", "va", "cat"];
 
   // Random integer generator
@@ -19,18 +19,27 @@ window.onload = function () {
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
   }
 
-  // string that will be displayed
+  // string that will be displayed; number of combos
   let output = "";
+  let numDomains = 0;
 
   // Nested for loops to get domain combos
   for (let i = 0; i < possList.length; i++) {
-        for (let j = 0; j < adjList.length; j++) {
-          for (let k = 0; k < nounList.length; k++) {
-            let extText = extList[getRandomIntInclusive(0, extList.length - 1)];
-            output += possList[i] + adjList[j] + nounList[k] + "." + extText + "<br>";
-          }
-        }
+    for (let j = 0; j < adjList.length; j++) {
+      for (let k = 0; k < nounList.length; k++) {
+        let extText = extList[getRandomIntInclusive(0, extList.length - 1)];
+        output +=
+          possList[i] + adjList[j] + nounList[k] + "." + extText + "<br>";
+        numDomains += 1;
       }
+      output += "<br>";
+    }
+  }
+
+  //Displaying the number of combos in h1
+  if (numDomains > 0) {
+    document.querySelector(".numDomains").innerHTML = numDomains;
+  }
 
   // displaying the results
   document.querySelector(".listHere").innerHTML = output;
